e6b19b6eb9809ff8a3ff534774dc5dae94d11ded
sumit <kumar1993sumit15@gmail.com>
2018-07-09 11:13:32 +0530

    register signup route

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-// app.use('/', require('./routes/'))
+app.use('/signup', require('./routes/signup'))
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
