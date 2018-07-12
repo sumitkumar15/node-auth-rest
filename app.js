b50cf3b3dc795dc0454cb5e3608f55b3353149c0
sumit <kumar1993sumit15@gmail.com>
2018-07-12 23:54:47 +0530

    added logging middleware & body-parser middleware

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
-app.use('/signup', require('./routes/signup'))
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use('/signup', require('./api/routes/signup'))
 app.get('/', (req, res) => {
   res.status(200).json('Hello World!')
 })
