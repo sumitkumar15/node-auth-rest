c89d93bcc195d9f8168d53c5f601d52c985d43c1
sumit <kumar1993sumit15@gmail.com>
2018-07-13 00:37:24 +0530

    add user model

--- api/models/user.js
+++ api/models/user.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 module.exports = mongoose.model('User', {
   _id: mongoose.Schema.Types.ObjectId,
-  email: String,
-  password: String
+  email: { type: String, required: true },
+  password: { type: String, required: true }
 })
