be0e02fe924245dcfc2bfb788a3b673f8a7edaff
sumit <kumar1993sumit15@gmail.com>
2018-07-13 02:29:30 +0530

    generate jwt token with login request

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
 
@@ -59,8 +60,17 @@ router.post('/login', (req, res) => {
             })
           } else {
             if (result) {
+              const token = jwt.sign({
+                email: user[0].email,
+                _id: user[0]._id
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: '1h'
+              })
               res.status(200).json({
-                message: 'Auth Success'
+                message: 'Auth Success',
+                token: token
               })
             } else {
               res.status(401).json({
