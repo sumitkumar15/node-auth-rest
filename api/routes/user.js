1d62c93e9eea93ec0de1d960b1f14a35565a1507
sumit <kumar1993sumit15@gmail.com>
2018-07-13 02:14:40 +0530

    add user login route

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,33 +6,89 @@ const bcrypt = require('bcrypt')
 const User = require('../models/user')
 
 router.post('/signup', (req, res) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
+  User.find({ email: req.body.email })
+    .then(user => {
+      if (user.length >= 1) {
+        // conflict
+        res.status(409).json({
+          message: 'email Exists'
+        })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({
+              error: err
+            })
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            })
+            user
+              .save()
+              .then(result => {
+                console.log(result)
+                res.status(201).json({
+                  message: 'User Created'
+                })
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                })
+              })
+          }
+        })
+      }
+    })
+})
+
+router.post('/login', (req, res) => {
+  User.find({ email: req.body.email })
+    .then(user => {
+      if (user.length < 1) {
+        res.status(401).json({
+          message: 'Auth failed'
+        })
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            res.status(401).json({
+              message: 'Auth failed'
+            })
+          } else {
+            if (result) {
+              res.status(200).json({
+                message: 'Auth Success'
+              })
+            } else {
+              res.status(401).json({
+                message: 'Auth failed'
+              })
+            }
+          }
+        })
+      }
+    })
+    .catch(err => {
       res.status(500).json({
         error: err
       })
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash
+    })
+})
+router.delete('/:userId', (req, res) => {
+  User.remove({ _id: req.param.userId })
+    .then(result => {
+      res.status(200).json({
+        message: 'User Deleted'
       })
-      user
-        .save()
-        .then(result => {
-          console.log(result)
-          res.status(201).json({
-            message: 'User Created'
-          })
-        })
-        .catch(err => {
-          res.status(500).json({
-            error: err
-          })
-        })
-    }
-  })
-
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 module.exports = router
