5569afc93cc6a686a0ca747684d3cf52154a71c0
sumit <kumar1993sumit15@gmail.com>
2018-07-13 12:36:35 +0530

    moved core logic to controllers

---
+++ api/controllers/user.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+exports.user_signup = (req, res, next) => {
+  User.find({ email: req.body.email })
+    .then(user => {
+      if (user.length >= 1) {
+        // conflict
+        res.status(409).json({
+          message: 'email Exists'
+        })
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            res.status(500).json({
+              error: err
+            })
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            })
+            user
+              .save()
+              .then(result => {
+                console.log(result)
+                res.status(201).json({
+                  message: 'User Created'
+                })
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                })
+              })
+          }
+        })
+      }
+    })
+}
+
+exports.user_login = (req, res, next) => {
+  User.find({ email: req.body.email })
+    .then(user => {
+      if (user.length < 1) {
+        res.status(401).json({
+          message: 'Auth failed'
+        })
+      } else {
+        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+          if (err) {
+            res.status(401).json({
+              message: 'Auth failed'
+            })
+          } else {
+            if (result) {
+              const token = jwt.sign({
+                email: user[0].email,
+                _id: user[0]._id
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: '1h'
+              })
+              res.status(200).json({
+                message: 'Auth Success',
+                token: token
+              })
+            } else {
+              res.status(401).json({
+                message: 'Auth failed'
+              })
+            }
+          }
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
+}
+
+exports.user_delete = (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .then(result => {
+      console.log(result)
+      if (result.n === 0) {
+        // throw exception
+      } else {
+        res.status(200).json({
+          message: 'User Deleted'
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: err
+      })
+    })
+}
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,113 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-
-const User = require('../models/user')
+const UserController = require('../controllers/user')
 
 const checkAuth = require('../middleware/auth-check')
 
-router.post('/signup', (req, res) => {
-  User.find({ email: req.body.email })
-    .then(user => {
-      if (user.length >= 1) {
-        // conflict
-        res.status(409).json({
-          message: 'email Exists'
-        })
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            res.status(500).json({
-              error: err
-            })
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash
-            })
-            user
-              .save()
-              .then(result => {
-                console.log(result)
-                res.status(201).json({
-                  message: 'User Created'
-                })
-              })
-              .catch(err => {
-                res.status(500).json({
-                  error: err
-                })
-              })
-          }
-        })
-      }
-    })
-})
+router.post('/signup', UserController.user_signup)
 
-router.post('/login', (req, res) => {
-  User.find({ email: req.body.email })
-    .then(user => {
-      if (user.length < 1) {
-        res.status(401).json({
-          message: 'Auth failed'
-        })
-      } else {
-        bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-          if (err) {
-            res.status(401).json({
-              message: 'Auth failed'
-            })
-          } else {
-            if (result) {
-              const token = jwt.sign({
-                email: user[0].email,
-                _id: user[0]._id
-              },
-              process.env.JWT_KEY,
-              {
-                expiresIn: '1h'
-              })
-              res.status(200).json({
-                message: 'Auth Success',
-                token: token
-              })
-            } else {
-              res.status(401).json({
-                message: 'Auth failed'
-              })
-            }
-          }
-        })
-      }
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      })
-    })
-})
-router.delete('/:userId', checkAuth, (req, res) => {
-  console.log(req.params)
-  User.remove({ _id: req.params.userId })
-    .then(result => {
-      console.log(result)
-      if (result.n === 0) {
-        // throw exception
-      } else {
-        res.status(200).json({
-          message: 'User Deleted'
-        })
-      }
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({
-        error: err
-      })
-    })
-})
+router.post('/login', UserController.user_login)
+
+router.delete('/:userId', checkAuth, UserController.user_delete)
 
 module.exports = router
